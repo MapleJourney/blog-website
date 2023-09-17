21b544a4ac57251f3aabf79e563551168d65fa3a
bzirs <bzirs@outlook.com>
2023-09-17 19:34:53 +0800

    feat: 修改配置

---
+++ .vuepress/config.ts
@@ -0,0 +1,48 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+import recoTheme from 'vuepress-theme-reco'
+
+export default defineUserConfig({
+	title: 'Maple Journey',
+	description: 'notes',
+	head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+	theme: recoTheme({
+		// 自动设置分类
+		autoSetSeries: true,
+		// home: '/',
+		style: '@vuepress-reco/style-default',
+
+		logo: '/logo.png',
+		author: 'aioxChina',
+		docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
+		docsBranch: 'main',
+		docsDir: 'example',
+		lastUpdatedText: '',
+		// series 为原 sidebar
+		series: {
+			'/docs/theme-reco/': [
+				{
+					text: 'module one',
+					children: ['home', 'theme']
+				},
+				{
+					text: 'module two',
+					children: ['api', 'plugin']
+				}
+			]
+		},
+		navbar: [
+			{ text: '首页', link: '/' },
+			{
+				text: '参考',
+				icon: 'Document',
+				children: [
+					{ text: 'Vuepress配置', link: 'https://v2.vuepress.vuejs.org/zh/reference/config.html' },
+					{ text: 'theme-reco配置', link: 'https://vuepress-theme-reco.recoluan.com/docs/theme/frontmatter-home.html' }
+				]
+			}
+		]
+	}),
+	port: 5467,
+	pagePatterns: ['**/*.md', '!**/README.md', '!.vuepress', '!node_modules']
+})
---
+++ index.md
@@ -0,0 +1,16 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: Maple Journey
+  description: 古之立大事者，不惟有超世之才，亦必有坚忍不拔之志
+  buttons:
+    - { text: 开始坐牢, link: '/docs' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/aioxChina' }
+
+---
